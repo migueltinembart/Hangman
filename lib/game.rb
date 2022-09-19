24f5be21040529357495ba10d257e2e9eb478048
Tinembart-sudo <miguel.tinembart@gmail.com>
2022-09-19 07:45:40 +0200

    Added classes for dependency
    - in game class added methods for gaining random word from list
    - added words as array via File.readlines

---
+++ lib/game.rb
@@ -0,0 +1,21 @@
+class Game
+
+    attr_reader :word_list
+
+    def load_text(file)
+        words = File.readlines(file)
+        words.map! do |line|
+            line.chomp 
+        end
+    end
+
+    def initialize(list)
+        @word_list = load_text(list)
+        puts word_list.length
+    end
+
+    def pick_random_word
+        random_number = rand(word_list.length) -1
+        return word_list[random_number]
+    end
+end
