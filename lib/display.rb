0903ea45dd2ef282c95a50b7cf1d032d6ffdf915
Tinembart-sudo <miguel.tinembart@gmail.com>
2022-09-24 15:02:00 +0200

    Added new line after each string

--- lib/display.rb
+++ lib/display.rb
@@ -1,47 +1,63 @@
 module Display
 
     def welcome_screen
-        "Welcome to Hangman"
+        "Welcome to Hangman\n"
     end
 
     def initialize_game_text
-        "Game initialized... "
+        "Game initialized...\n"
     end
 
     def ask_name_of_player
-        "What is your name"
+        "What is your name\n"
     end
 
     def how_many_turns(tries)
-        "you start with #{tries} tries"
+        "you start with #{tries} tries\n"
     end
 
     def how_many_turns_left(tries)
-        "you got #{tries} left..."
+        "you got #{tries} left...\n"
     end
 
     def hit(char)
-        "you guessed #{char} right, Nice!"
+        "you guessed #{char} right, Nice!\n"
     end
 
     def show_board(word_array)
-        word_array.join(" ")
+        "#{word_array.join(" ")}"
     end
 
     def ask_for_pick
-        "Pick a character..."
+        "Pick a character...\n"
     end
 
     def wrong
-        "Wrong input you lose a life"
+        "Wrong input you lose a life\n"
     end
 
     def invalid_input
-        "invalid input"
+        "invalid input\n"
     end
-    
+
     def game_over_screen
-        "You lost the game... wanna retry?"
+        "You lose\n"
+    end
+
+    def duplicate_character
+        "you cannot type character already passed\n"
+    end
+
+    def correct_word(word)
+        "the correct answer would have been... #{word}\n"
+    end
+
+    def winning_screen
+        "You won\n"
+    end
+
+    def saving_game_in_progress
+        "Your save state has been has been saved...\n"
     end
 
 
